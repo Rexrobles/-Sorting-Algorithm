d304bef5f963b383ec7da383a6938be95dfe0725
Rexrobles <reximma03@gmail.com>
2023-01-23 01:40:09 +0800

    implemented for loop that will search the smallest number in the unsorted part of the list.

--- selection-sort.py
+++ selection-sort.py
@@ -8,3 +8,8 @@ numbers = [96, 20, 67, 27, 38, 7, 33, 43, 21, 39]
 print(f"\nUnsorted Numbers: {numbers}")
 
 def sort(numbers):
+    for elements in range(9):
+        minpos = elements
+    for minNUM in range (elements,11):
+        if numbers[minNUM] <numbers [minpos]:
+            minpos = minNUM
