c39ad53d650b08fdda3e009b7ea05b558dbaf1c0
Rexrobles <reximma03@gmail.com>
2023-01-24 01:10:04 +0800

    add an array containing 10 numbers and printed it

--- quick-sort.py
+++ quick-sort.py
@@ -7,3 +7,7 @@
 # Repeat the process, stop when index of itemFromLeft is greater than index of itemFromRight
 # Swap the pivot with itemFromLeft
 # repeat until all element are sorted
+
+numbers = [96, 20, 67, 27, 38, 7, 33, 43, 21, 39] # unsorted list of numbers
+
+print(f"\nUnsorted Numbers: {numbers}\n")
