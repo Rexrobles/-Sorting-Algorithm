9817b46badc28c38cb8a374297f9cbe4cabcaed4
Rexrobles <reximma03@gmail.com>
2023-01-23 15:40:27 +0800

    define how insertion sort run

---
+++ insertion-sort.py
@@ -0,0 +1,4 @@
+# INSERTION SORT
+# - consider first element is sorted and the rest is unsorted
+# - pick the first element in the unsorted part and place it in its correction position in the sorted part
+# - repeat the steps
