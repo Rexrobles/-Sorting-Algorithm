b4925386915a359b6c9bf86f71dadc2e063734a9
Rexrobles <reximma03@gmail.com>
2023-01-23 15:21:54 +0800

    For loop, if the condition satisfied it Swapped numbers

--- bubble-sort.py
+++ bubble-sort.py
@@ -9,3 +9,9 @@ numbers = [96, 20, 67, 27, 38, 7, 33, 43, 21, 39]
 print(f"\nUnsorted Numbers: {numbers}\n")
 
 def sort(numbers):
+    for element in range(len(numbers)-1, 0, -1):
+        for maxNUM in range(element):
+            if numbers[maxNUM]>numbers[maxNUM+1]:
+                temp = numbers[maxNUM]
+                numbers[maxNUM] = numbers[maxNUM+1]
+                numbers[maxNUM+1] = temp
