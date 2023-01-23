167338709d818b313061508afe93a68047502c3e
Rexrobles <reximma03@gmail.com>
2023-01-23 21:33:47 +0800

    add a list containing 10 numbers and printed it

--- merge-sort.py
+++ merge-sort.py
@@ -3,3 +3,7 @@
 # - keep dividing until the arrays becomes an individual items only
 # - merge the single left part with its other half
 # - keep merging until it becomes sorted
+
+numbers = [96, 20, 67, 27, 38, 7, 33, 43, 21, 39] # unsorted list of numbers
+
+print(f"\nUnsorted Numbers: {numbers}\n")
