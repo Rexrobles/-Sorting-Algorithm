791ecf247bf4612891dd7f3977250bcf8b39caf5
Rexrobles <reximma03@gmail.com>
2023-01-23 15:00:23 +0800

    add an array  containing 10 numbers and printed it

--- bubble-sort.py
+++ bubble-sort.py
@@ -4,3 +4,6 @@
 # - if first element is less than second element then leave it as is
 # - repeat steps excluding the sorted larger elements in the right part of the array
 
+numbers = [96, 20, 67, 27, 38, 7, 33, 43, 21, 39]
+
+print(f"\nUnsorted Numbers: {numbers}\n")
