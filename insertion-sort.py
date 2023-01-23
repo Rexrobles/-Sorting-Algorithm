740a9cfb6d22f8cc624d5a794ca24eba4dfad2a2
Rexrobles <reximma03@gmail.com>
2023-01-23 15:42:00 +0800

    add a list containing 10 numbers and printed it

--- insertion-sort.py
+++ insertion-sort.py
@@ -2,3 +2,7 @@
 # - consider first element is sorted and the rest is unsorted
 # - pick the first element in the unsorted part and place it in its correction position in the sorted part
 # - repeat the steps
+
+numbers = [96, 20, 67, 27, 38, 7, 33, 43, 21, 39] # unsorted list of numbers
+
+print(f"\nUnsorted Numbers: {numbers}\n")
