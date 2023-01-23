ae3101524f182e726f9c6f9c7f238895eb8ef1c8
Rexrobles <reximma03@gmail.com>
2023-01-23 14:59:02 +0800

    define how bubble sort run

---
+++ bubble-sort.py
@@ -0,0 +1,6 @@
+# BUBBLE SORT
+# - compare two adjacent elements
+# - if first element is greater than  second element then Swap
+# - if first element is less than second element then leave it as is
+# - repeat steps excluding the sorted larger elements in the right part of the array
+
