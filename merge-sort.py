5ac9896a078763ab152523a0b023109327054df4
Rexrobles <reximma03@gmail.com>
2023-01-23 21:46:07 +0800

    def merge_sort and sepation

--- merge-sort.py
+++ merge-sort.py
@@ -7,3 +7,12 @@
 numbers = [96, 20, 67, 27, 38, 7, 33, 43, 21, 39] # unsorted list of numbers
 
 print(f"\nUnsorted Numbers: {numbers}\n")
+
+def merge_sort(numbers):
+    if len(numbers) > 1:
+        # Dividing into two parts
+        LeftPart = numbers[:len(numbers)//2]
+        RightPart = numbers[len(numbers)//2:]
+
+        merge_sort(LeftPart)
+        merge_sort(RightPart)
