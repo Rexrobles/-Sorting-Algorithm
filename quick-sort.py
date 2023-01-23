12a4f06482500145d1a7ba7b3458017b5dec4248
Rexrobles <reximma03@gmail.com>
2023-01-24 01:02:01 +0800

    define how quick-sort run

--- quick-sort.py
+++ quick-sort.py
@@ -1,1 +1,9 @@
 # QUICK SORT
+# choose any element as pivot
+# move the pivot to the end of array
+# look for itemFromLeft that is larger than pivot
+# look for itemFromRight that is smaller than pivot
+# swap the item from left to item from right
+# Repeat the process, stop when index of itemFromLeft is greater than index of itemFromRight
+# Swap the pivot with itemFromLeft
+# repeat until all element are sorted
