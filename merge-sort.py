05da337c4e7d93d06c06d085241b8d18a05b9c4c
Rexrobles <reximma03@gmail.com>
2023-01-23 21:23:23 +0800

    define how merge sort run

---
+++ merge-sort.py
@@ -0,0 +1,5 @@
+# MERGE SORT
+# - divide the array into left and right part
+# - keep dividing until the arrays becomes an individual items only
+# - merge the single left part with its other half
+# - keep merging until it becomes sorted
