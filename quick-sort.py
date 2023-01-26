5930b926f30ffed473f310d67babd0db41645178
Rexrobles <reximma03@gmail.com>
2023-01-27 01:34:52 +0800

    undo the last commit

--- quick-sort.py
+++ quick-sort.py
@@ -1,8 +1,8 @@
 # QUICK SORT
 # choose any element as pivot
 # move the pivot to the end of array
-# look for itemFromLeft that is smaller than pivot
-# look for itemFromRight that is than larger pivot
+# look for itemFromLeft that is larger than pivot
+# look for itemFromRight that is smaller than pivot
 # swap the itemfromleft to itemfromright
 # Repeat the process, stop when index of itemFromLeft is greater than index of itemFromRight
 # Swap the pivot with itemFromLeft
